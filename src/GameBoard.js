b84b48588835126f6ab3dd0afa17d8d19771204a
Christian Wunder <wunderman4@gmail.com>
2018-04-11 07:54:16 -0700

    cleaned up user moves code

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,117 +1,119 @@
-import React, { Component } from "react";
-import * as C from "./Constants.js";
-import Tile from "./Tile.js";
-import { rotate, move } from "./MoveLogic.js";
-import { getBoard } from "./util";
-import io from "socket.io-client";
+import React, { Component } from 'react';
+import * as C from './Constants.js';
+import Tile from './Tile.js';
+import { rotate, move } from './MoveLogic.js';
+import { getBoard } from './util';
+import io from 'socket.io-client';
 
 var socket;
 
 class GameBoard extends Component {
-  state = {
-    tiles: getBoard()
-  };
+	state = {
+		tiles: getBoard()
+	};
 
-  // togleStep = await () => {
-  //   return !this.state.singleStep;
-  // };
+	// togleStep = await () => {
+	//   return !this.state.singleStep;
+	// };
 
-  timeControl() {}
+	timeControl() {}
 
-  // userChoice is where we send user move.
-  userChoice = e => {
-    // if (!timeControl(e, this.props.beenClicked, this.props.autoPlay)) {
-    //   // await button click to move a piece
-    //   return;
-    // }
+	// userChoice is where we send user move.
+	userChoice = e => {
+		// if (!timeControl(e, this.props.beenClicked, this.props.autoPlay)) {
+		//   // await button click to move a piece
+		//   return;
+		// }
 
-    //console.log(e);
-    const userState =
-      this.state.tiles.find(tile => tile.player === C.PLAYER_USER) || {};
+		//console.log(e);
+		const userState =
+			this.state.tiles.find(tile => tile.player === C.PLAYER_USER) || {};
 
-    var direction = userState.direction; // used to set new direction of user.
+		var direction = userState.direction; // used to set new direction of user.
 
-    switch (e) {
-      case C.DIRECTION_CLOCKWISE:
-        direction = rotate(C.DIRECTION_CLOCKWISE, userState);
-        break;
-      case C.MOVE_MAKE:
-        move(userState, this.state.tiles);
-        break;
-      case C.DIRECTION_COUNTERCLOCKWISE:
-        direction = rotate(C.DIRECTION_COUNTERCLOCKWISE, userState);
-        break;
-      default:
-        direction = userState.direction;
-        break;
-    }
+		switch (e) {
+			case C.DIRECTION_CLOCKWISE:
+				direction = rotate(C.DIRECTION_CLOCKWISE, userState);
+				break;
+			case C.MOVE_MAKE:
+				move(userState, this.state.tiles);
+				break;
+			case C.DIRECTION_COUNTERCLOCKWISE:
+				direction = rotate(C.DIRECTION_COUNTERCLOCKWISE, userState);
+				break;
+			default:
+				direction = userState.direction;
+				break;
+		}
 
-    // old key press moves
-    // switch (e.key) {
-    //   case "ArrowRight":
-    //     socket.emit("response", { GameBoard: this.state.tiles });
-    //     break;
-    //   //   case "ArrowUp":
-    //   //     move(userState, this.state.tiles);
-    //   //     break;
-    //   //   case "ArrowLeft":
-    //   //     direction = rotate(C.DIRECTION_COUNTERCLOCKWISE, userState);
-    //   //     break;
-    //   default:
-    //     direction = userState.direction;
-    //     break;
-    // }
-    const newBoard = this.state.tiles.map(
-      tile =>
-        tile.player === C.PLAYER_USER ? { ...tile, direction: direction } : tile
-    );
-    this.setState(
-      {
-        tiles: newBoard
-      },
-      () => {
-        // socket.send needs to go here to send updated gameboard
-        setTimeout(() => {
-          socket.emit("response", { GameBoard: this.state.tiles });
-        }, 1000);
-      }
-    );
-  };
+		// Uncomment to enable player move by key press.
+		// switch (e.key) {
+		// 	case 'ArrowRight':
+		// 		//socket.emit('response', { GameBoard: this.state.tiles });
+		// 		direction = rotate(C.DIRECTION_CLOCKWISE, userState);
+		// 		break;
+		// 	case 'ArrowUp':
+		// 		move(userState, this.state.tiles);
+		// 		break;
+		// 	case 'ArrowLeft':
+		// 		direction = rotate(C.DIRECTION_COUNTERCLOCKWISE, userState);
+		// 		break;
+		// 	default:
+		// 		direction = userState.direction;
+		// 		break;
+		// }
 
-  componentDidMount() {
-    // Open socket.io connection here
-    window.gameBoard = this.state.tiles;
-    window.addEventListener("keydown", this.userChoice);
-    // socket.io
-    socket = io("http://localhost:9090");
-    socket.on("move", this.userChoice);
-  }
+		const newBoard = this.state.tiles.map(
+			tile =>
+				tile.player === C.PLAYER_USER ? { ...tile, direction: direction } : tile
+		);
+		this.setState(
+			{
+				tiles: newBoard
+			},
+			() => {
+				// socket.send needs to go here to send updated gameboard
+				setTimeout(() => {
+					socket.emit('response', { GameBoard: this.state.tiles });
+				}, 1000);
+			}
+		);
+	};
 
-  componentWillReceiveProps(nextProps) {
-    if (!this.props.autoPlay && nextProps.autoPlay) {
-      timeControl();
-    }
+	componentDidMount() {
+		// Open socket.io connection here
+		window.gameBoard = this.state.tiles;
+		window.addEventListener('keydown', this.userChoice);
+		// socket.io
+		socket = io('http://localhost:9090');
+		socket.on('move', this.userChoice);
+	}
 
-    if (this.props.autoPlay && !nextProps.autoPlay) {
-      // stopTimeControl();
-    }
-  }
-  render() {
-    return (
-      <div className="board">
-        {this.state.tiles.map(t => (
-          <Tile
-            key={`${t.x}-${t.y}`}
-            player={t.player}
-            direction={t.direction}
-            x={t.x}
-            y={t.y}
-            //size={}
-          />
-        ))}
-      </div>
-    );
-  }
+	componentWillReceiveProps(nextProps) {
+		if (!this.props.autoPlay && nextProps.autoPlay) {
+			timeControl();
+		}
+
+		if (this.props.autoPlay && !nextProps.autoPlay) {
+			// stopTimeControl();
+		}
+	}
+	render() {
+		return (
+			<div className="board">
+				{this.state.tiles.map(t => (
+					<Tile
+						key={`${t.x}-${t.y}`}
+						player={t.player}
+						direction={t.direction}
+						x={t.x}
+						y={t.y}
+						//size={}
+					/>
+				))}
+			</div>
+		);
+	}
 }
 
 export default GameBoard;
--- src/MoveLogic.js
+++ src/MoveLogic.js
@@ -1,151 +1,151 @@
-import * as C from "./Constants.js";
+import * as C from './Constants.js';
 
 export function rotate(input, us) {
-  // input == usermove, us == UserState
-  let newDirection = "";
-  switch (us.direction) {
-    case C.DIRECTION_NORTH:
-      if (input === "clockwise") {
-        newDirection = C.DIRECTION_EAST;
-      } else {
-        newDirection = C.DIRECTION_WEST;
-      }
-      break;
-    case C.DIRECTION_SOUTH:
-      if (input === "clockwise") {
-        newDirection = C.DIRECTION_WEST;
-      } else {
-        newDirection = C.DIRECTION_EAST;
-      }
-      break;
-    case C.DIRECTION_EAST:
-      if (input === "clockwise") {
-        newDirection = C.DIRECTION_SOUTH;
-      } else {
-        newDirection = C.DIRECTION_NORTH;
-      }
-      break;
-    case C.DIRECTION_WEST:
-      if (input === "clockwise") {
-        newDirection = C.DIRECTION_NORTH;
-      } else {
-        newDirection = C.DIRECTION_SOUTH;
-      }
-      break;
-    default:
-      break;
-  }
-  return newDirection;
+	// input == usermove, us == UserState
+	let newDirection = '';
+	switch (us.direction) {
+		case C.DIRECTION_NORTH:
+			if (input === 'clockwise') {
+				newDirection = C.DIRECTION_EAST;
+			} else {
+				newDirection = C.DIRECTION_WEST;
+			}
+			break;
+		case C.DIRECTION_SOUTH:
+			if (input === 'clockwise') {
+				newDirection = C.DIRECTION_WEST;
+			} else {
+				newDirection = C.DIRECTION_EAST;
+			}
+			break;
+		case C.DIRECTION_EAST:
+			if (input === 'clockwise') {
+				newDirection = C.DIRECTION_SOUTH;
+			} else {
+				newDirection = C.DIRECTION_NORTH;
+			}
+			break;
+		case C.DIRECTION_WEST:
+			if (input === 'clockwise') {
+				newDirection = C.DIRECTION_NORTH;
+			} else {
+				newDirection = C.DIRECTION_SOUTH;
+			}
+			break;
+		default:
+			break;
+	}
+	return newDirection;
 }
 
 export function move(us, gb) {
-  // input = usermove, us == userState, gb  == gameBoard
-  let targetData = {
-    Player: "",
-    Direction: "",
-    XPos: "",
-    YPos: ""
-  };
+	// input = usermove, us == userState, gb  == gameBoard
+	let targetData = {
+		Player: '',
+		Direction: '',
+		XPos: '',
+		YPos: ''
+	};
 
-  // Gathering target position data.
-  if (BoundsCheck(us, gb)) {
-    switch (us.direction) {
-      case C.DIRECTION_NORTH:
-        // Gather properties of tile South of current position
-        gb.forEach(tile => {
-          if (tile.x === us.x && tile.y === us.y - 1) {
-            targetData = targeting(targetData, tile);
-          }
-        });
-        break;
-      case C.DIRECTION_SOUTH:
-        // Gather properties of tile North of current position
-        gb.forEach(tile => {
-          if (tile.x === us.x && tile.y === us.y + 1) {
-            targetData = targeting(targetData, tile);
-          }
-        });
-        break;
-      case C.DIRECTION_EAST:
-        //Gather properties of tile West of current position
-        gb.forEach(tile => {
-          if (tile.x === us.x + 1 && tile.y === us.y) {
-            targetData = targeting(targetData, tile);
-          }
-        });
-        break;
-      case C.DIRECTION_WEST:
-        // Gather properties of tile East of current position
-        gb.forEach(tile => {
-          if (tile.x === us.x - 1 && tile.y === us.y) {
-            targetData = targeting(targetData, tile);
-          }
-        });
-        break;
-      default:
-        break;
-    }
+	// Gathering target position data.
+	if (BoundsCheck(us, gb)) {
+		switch (us.direction) {
+			case C.DIRECTION_NORTH:
+				// Gather properties of tile South of current position
+				gb.forEach(tile => {
+					if (tile.x === us.x && tile.y === us.y - 1) {
+						targetData = targeting(targetData, tile);
+					}
+				});
+				break;
+			case C.DIRECTION_SOUTH:
+				// Gather properties of tile North of current position
+				gb.forEach(tile => {
+					if (tile.x === us.x && tile.y === us.y + 1) {
+						targetData = targeting(targetData, tile);
+					}
+				});
+				break;
+			case C.DIRECTION_EAST:
+				//Gather properties of tile West of current position
+				gb.forEach(tile => {
+					if (tile.x === us.x + 1 && tile.y === us.y) {
+						targetData = targeting(targetData, tile);
+					}
+				});
+				break;
+			case C.DIRECTION_WEST:
+				// Gather properties of tile East of current position
+				gb.forEach(tile => {
+					if (tile.x === us.x - 1 && tile.y === us.y) {
+						targetData = targeting(targetData, tile);
+					}
+				});
+				break;
+			default:
+				break;
+		}
 
-    if (targetData.Direction === C.DIRECTION_NULL) {
-      eatUm(us, gb, targetData);
-    } else if (
-      us.direction === C.DIRECTION_NORTH &&
-      targetData.Direction !== C.DIRECTION_SOUTH
-    ) {
-      eatUm(us, gb, targetData);
-    } else if (
-      us.direction === C.DIRECTION_SOUTH &&
-      targetData.Direction !== C.DIRECTION_NORTH
-    ) {
-      eatUm(us, gb, targetData);
-    } else if (
-      us.direction === C.DIRECTION_EAST &&
-      targetData.Direction !== C.DIRECTION_WEST
-    ) {
-      eatUm(us, gb, targetData);
-    } else if (
-      us.direction === C.DIRECTION_WEST &&
-      targetData.Direction !== C.DIRECTION_EAST
-    ) {
-      eatUm(us, gb, targetData);
-    } else {
-      // bounce
-    }
-  }
+		if (targetData.Direction === C.DIRECTION_NULL) {
+			eatUm(us, gb, targetData);
+		} else if (
+			us.direction === C.DIRECTION_NORTH &&
+			targetData.Direction !== C.DIRECTION_SOUTH
+		) {
+			eatUm(us, gb, targetData);
+		} else if (
+			us.direction === C.DIRECTION_SOUTH &&
+			targetData.Direction !== C.DIRECTION_NORTH
+		) {
+			eatUm(us, gb, targetData);
+		} else if (
+			us.direction === C.DIRECTION_EAST &&
+			targetData.Direction !== C.DIRECTION_WEST
+		) {
+			eatUm(us, gb, targetData);
+		} else if (
+			us.direction === C.DIRECTION_WEST &&
+			targetData.Direction !== C.DIRECTION_EAST
+		) {
+			eatUm(us, gb, targetData);
+		} else {
+			// bounce
+		}
+	}
 }
 // ----------------------------------------------------------
 
 // Checks the outer bounds of the game board
 function BoundsCheck(us, gb) {
-  const lastPos = gb[gb.length - 1];
-  if (us.x === 0 && us.direction === C.DIRECTION_WEST) {
-    return false;
-  } else if (us.x === lastPos.x && us.direction === C.DIRECTION_EAST) {
-    return false;
-  } else if (us.y === 0 && us.direction === C.DIRECTION_NORTH) {
-    return false;
-  } else if (us.y === lastPos.y && us.direction === C.DIRECTION_SOUTH) {
-    return false;
-  } else {
-    return true;
-  }
+	const lastPos = gb[gb.length - 1];
+	if (us.x === 0 && us.direction === C.DIRECTION_WEST) {
+		return false;
+	} else if (us.x === lastPos.x && us.direction === C.DIRECTION_EAST) {
+		return false;
+	} else if (us.y === 0 && us.direction === C.DIRECTION_NORTH) {
+		return false;
+	} else if (us.y === lastPos.y && us.direction === C.DIRECTION_SOUTH) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
 function targeting(targetData, tile) {
-  targetData.XPos = tile.x;
-  targetData.YPos = tile.y;
-  targetData.Direction = tile.direction;
-  targetData.Player = tile.player;
-  return targetData;
+	targetData.XPos = tile.x;
+	targetData.YPos = tile.y;
+	targetData.Direction = tile.direction;
+	targetData.Player = tile.player;
+	return targetData;
 }
 
 // eats the player
 function eatUm(us, gb, targetData) {
-  const tile = gb.find(tile => {
-    return tile.x === targetData.XPos && tile.y === targetData.YPos;
-  });
-  tile.direction = us.direction;
-  tile.player = C.PLAYER_USER;
-  us.player = C.PLAYER_OPEN;
-  us.direction = C.DIRECTION_NULL;
+	const tile = gb.find(tile => {
+		return tile.x === targetData.XPos && tile.y === targetData.YPos;
+	});
+	tile.direction = us.direction;
+	tile.player = C.PLAYER_USER;
+	us.player = C.PLAYER_OPEN;
+	us.direction = C.DIRECTION_NULL;
 }
