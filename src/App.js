3cc6f0e4ab39692a912fde3763cb26e0573110d8
Christian Wunder <wunderman4@gmail.com>
2018-04-11 06:38:32 -0700

    updated Electron to 1.8.4

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React, { Component } from "react";
-import "../css/App.css";
-import GameBoard from "./GameBoard.js";
-import { manualMode } from "./util.js";
+import React, { Component } from 'react';
+import '../css/App.css';
+import GameBoard from './GameBoard.js';
+import { manualMode } from './util.js';
 
 class App extends Component {
-  state = {
-    toggle: false,
-    beenClicked: false
-  };
+	state = {
+		toggle: false,
+		beenClicked: false
+	};
 
-  resetBeenClicked = () => this.setState({ beenClicked: false });
+	resetBeenClicked = () => this.setState({ beenClicked: false });
 
-  render() {
-    return (
-      <div className="App">
-        <div className="banner">
-          <h1 className="title">CODE CRITTERS</h1>
-        </div>
-        <GameBoard
-          autoPlay={this.state.toggle}
-          beenClicked={this.state.beenClicked}
-          resetBeenClicked={this.resetBeenClicked}
-        />
-        <div>
-          <button className="ConBtn">Step</button>
-          <label className="switch">
-            <input
-              checked={this.state.toggle}
-              onChange={() => {
-                this.setState({ toggle: !this.state.toggle });
-              }}
-              type="checkbox"
-            />
-            <span className="slider round" />
-          </label>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="banner">
+					<h1 className="title">CODE CRITTERS</h1>
+				</div>
+				<GameBoard
+					autoPlay={this.state.toggle}
+					beenClicked={this.state.beenClicked}
+					resetBeenClicked={this.resetBeenClicked}
+				/>
+				<div>
+					<button className="ConBtn">Step</button>
+					<label className="switch">
+						<input
+							checked={this.state.toggle}
+							onChange={() => {
+								this.setState({ toggle: !this.state.toggle });
+							}}
+							type="checkbox"
+						/>
+						<span className="slider round" />
+					</label>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Tile.js
+++ src/Tile.js
@@ -1,28 +1,25 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { boardStyle } from "./util.js";
-
-// This is the second try where I made it a stateless component. I get the classnames for player, direction,
-//
+import React from 'react';
+import PropTypes from 'prop-types';
+import { boardStyle } from './util.js';
 
 const Tile = props => {
-  const { height, width } = boardStyle();
-  return (
-    <div
-      key={`${props.x}-${props.y}`}
-      style={{ flex: `0 0 ${width}%`, height: `${height}vh` }}
-      className="square"
-    >
-      <div className={`circle ${props.player} ${props.direction}`} />
-    </div>
-  );
+	const { height, width } = boardStyle();
+	return (
+		<div
+			key={`${props.x}-${props.y}`}
+			style={{ flex: `0 0 ${width}%`, height: `${height}vh` }}
+			className="square"
+		>
+			<div className={`circle ${props.player} ${props.direction}`} />
+		</div>
+	);
 };
 
 Tile.PropTypes = {
-  player: PropTypes.string.isRequired,
-  direction: PropTypes.string.isRequired,
-  x: PropTypes.number.isRequired,
-  y: PropTypes.number.isRequired
+	player: PropTypes.string.isRequired,
+	direction: PropTypes.string.isRequired,
+	x: PropTypes.number.isRequired,
+	y: PropTypes.number.isRequired
 };
 
 export default Tile;
